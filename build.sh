a1733b01a32646af3319958ab5f87d9f7e166805
Meisie Widuri <ewprjkt@proton.me>
2026-02-01 02:47:01 +0700

    Create build.sh for ROM building process
    Add build script for building ROM with necessary commands.

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash                                           
+                                                      
+cyan='\033[0;36m'                                     
+blue='\033[0;34m'                                     
+nocol='\033[0m'                                       
+                                                      
+#set -e                                               
+                                                      
+clear                                                 
+                                                      
+export TZ=Asia/Jakarta                                
+                                                      
+crave clone destroy -y /crave-devspaces/vos           
+                                                      
+crave clone create --projectID 35 /crave-devspaces/vos
+                                                      
+cd /crave-devspaces/vos                               
+                                                      
+echo -e "$cyan***************************************>
+echo -e "       BUILDING ROM with Bomb ass command   >
+echo -e "********************************************>
+                                                      
+crave run --no-patch -- "rm -rf .repo/local_manifests>
+repo init -u https://github.com/VoltageOS/manifest.gi>
+git clone --depth=1 https://github.com/Tiktodz/local_>
+/opt/crave/resync.sh; \                               
+. build/envsetup.sh; \                                
+export TZ=Asia/Jakarta; \                             
+brunch X00TD"
