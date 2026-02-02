93081ab2a4ea07c332ee49b2f94e1598d2c724a5
Meisie Widuri <ewprjkt@proton.me>
2026-02-02 09:24:00 +0700

    Uncomment build commands in build.sh
    Uncomment build commands and add echo statements for clarity.

--- build.sh
+++ build.sh
@@ -3,26 +3,27 @@
 cyan='\033[0;36m'
 blue='\033[0;34m'
 nocol='\033[0m'
+
 #set -e                                               
 
 clear
 
 export TZ=Asia/Jakarta
 
-#crave clone destroy -y /crave-devspaces/vos
+crave clone destroy -y /crave-devspaces/vos
 
-#crave clone create --projectID 35 /crave-devspaces/vos
+crave clone create --projectID 35 /crave-devspaces/vos
 
-#cd /crave-devspaces/vos
+cd /crave-devspaces/vos
 
-#echo -e "$cyan***************************************>
-#echo -e "       BUILDING ROM with Bomb ass command   >
-#echo -e "********************************************>
-                                                      
-rm -rf .repo/local_manifests; \
+#echo -e "$cyan***************************************"
+#echo -e "       BUILDING ROM with Bomb ass command   "
+#echo -e "********************************************"
+
+crave run --no-patch -- "rm -rf .repo/local_manifests; \
 repo init -u https://github.com/VoltageOS/manifest.git -b 16.2 --git-lfs; \
 git clone --depth=1 https://github.com/Tiktodz/local_manifesf -b vos .repo/local_manifests; \
 /opt/crave/resync.sh; \
 . build/envsetup.sh; \
 export TZ=Asia/Jakarta; \
-brunch X00TD
+brunch X00TD"
