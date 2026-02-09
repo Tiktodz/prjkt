005aaeca8b0501351f1d816f993f6c508e875b69
Meisie Widuri <dotkit@electrowizard.me>
2026-02-09 21:27:38 +0700

    Enhance build script with key setup and kernel script
    Added commands to set up lineage-priv keys and kernel.

--- build.sh
+++ build.sh
@@ -20,10 +20,21 @@ echo -e "$cyan***************************************"
 echo -e "       BUILDING ROM with Bomb ass command   "
 echo -e "********************************************"
 
-rm -rf .repo/local_manifests; \
-repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs; \
-git clone --depth=1 https://github.com/Tiktodz/local_manifest -b los .repo/local_manifests; \
-/opt/crave/resync.sh; \
-. build/envsetup.sh; \
-export TZ=Asia/Jakarta; \
+rm -rf .repo/local_manifests
+repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
+git clone --depth=1 https://github.com/Tiktodz/local_manifest -b los .repo/local_manifests
+/opt/crave/resync.sh
+. build/envsetup.sh
+rm -rf vendor/lineage-priv/keys
+mkdir -p vendor/lineage-priv/keys
+cp scripts/lineage-priv-template/* vendor/lineage-priv/keys/
+cd vendor/lineage-priv/keys
+./keys.sh
+cd -
+cd kernel/asus/sdm660
+curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s main
+cd -
 brunch X00TD
+
+
+###myhanaqueen:*###
