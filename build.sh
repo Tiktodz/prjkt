03b2dd04223868bde88bf1759bc589590c3e0892
Meisie Widuri <dotkit@electrowizard.me>
2026-02-11 09:18:58 +0700

    Modify build.sh for crdroid project setup
    Updated build script to use crdroid repository and project.

--- build.sh
+++ build.sh
@@ -10,30 +10,30 @@ clear
 
 export TZ=Asia/Jakarta
 
-#crave clone destroy -y /crave-devspaces/vos
+#crave clone destroy -y /crave-devspaces/los
 
-#crave clone create --projectID 35 /crave-devspaces/los
+#crave clone create --projectID 35 /crave-devspaces/crd
 
-#cd /crave-devspaces/los
+#cd /crave-devspaces/crd
 
 echo -e "$cyan***************************************"
 echo -e "       BUILDING ROM with Bomb ass command   "
 echo -e "********************************************"
 
-rm -rf .repo/local_manifests out/soong out/host/linux-x86
-repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
-git clone --depth=1 https://github.com/Tiktodz/local_manifest -b los .repo/local_manifests
+rm -rf .repo/local_manifests
+repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
+git clone --depth=1 https://github.com/Tiktodz/local_manifest -b crd .repo/local_manifests
 /opt/crave/resync.sh
 . build/envsetup.sh
-rm -rf vendor/lineage-priv/keys
-mkdir -p vendor/lineage-priv/keys
-cp -R lineage/scripts/lineage-priv-template/* vendor/lineage-priv/keys/
-cd vendor/lineage-priv/keys
-./keys.sh
-cd -
-cd kernel/asus/sdm660
-curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s main
-cd -
+#rm -rf vendor/lineage-priv/keys
+#mkdir -p vendor/lineage-priv/keys
+#cp -R lineage/scripts/lineage-priv-template/* vendor/lineage-priv/keys/
+#cd vendor/lineage-priv/keys
+#./keys.sh
+#cd -
+#cd kernel/asus/sdm660
+#curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s main
+#cd -
 brunch X00TD
 
 
