a5f83259576a58aaa90aedd90e75f9fb559998d1
Tiktodz <dotkit@electrowizard.me>
2026-03-23 09:49:15 +0000

    build: regenerate
    Signed-off-by: Tiktodz <dotkit@electrowizard.me>

--- build.sh
+++ build.sh
@@ -4,37 +4,26 @@ cyan='\033[0;36m'
 blue='\033[0;34m'
 nocol='\033[0m'
 
-#set -e                                               
+#set -e
 
 clear
 
 export TZ=Asia/Jakarta
 
-#crave clone destroy -y /crave-devspaces/los
+#crave clone destroy -y /crave-devspaces/tytyd
 
-#crave clone create --projectID 35 /crave-devspaces/crd
+#crave clone create --projectID 93 /crave-devspaces/tytyd
 
-#cd /crave-devspaces/crd
+#cd /crave-devspaces/tytyd
 
 echo -e "$cyan***************************************"
 echo -e "       BUILDING ROM with Bomb ass command   "
 echo -e "********************************************"
 
-rm -rf .repo/local_manifests
-repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
-git clone --depth=1 https://github.com/Tiktodz/local_manifest -b crd .repo/local_manifests
+rm -rf .repo/local_manifests out/soong out/host/linux-x86
+repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
+git clone --depth=1 https://github.com/Tiktodz/local_manifest -b custom .repo/local_manifests
 /opt/crave/resync.sh
 . build/envsetup.sh
-#rm -rf vendor/lineage-priv/keys
-#mkdir -p vendor/lineage-priv/keys
-#cp -R lineage/scripts/lineage-priv-template/* vendor/lineage-priv/keys/
-#cd vendor/lineage-priv/keys
-#./keys.sh
-#cd -
-#cd kernel/asus/sdm660
-#curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s main
-#cd -
-brunch X00TD
-
-
-###myhanaqueen:*###
+lunch lineage_X00TD-bp4a-userdebug
+mka bacon
